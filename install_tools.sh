8fea416735ff9f58f4dff5a9d5f3be6a1ee8795c
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2023-12-04 11:53:08 -0500

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -5,6 +5,7 @@ sudo apt install htop git-gui libnotify4 libnspr4 libnss3 libsecret-1-0 libsecre
 
 git config --global user.name "Ankit Bhardwaj"
 git config --global user.email "bhrdwj.ankit@gmail.com"
+git config core.editor vim
 
 wget https://az764295.vo.msecnd.net/stable/ae08d5460b5a45169385ff3fd44208f431992451/code_1.42.0-1580986622_amd64.deb
 sudo dpkg -i code_1.42.0-1580986622_amd64.deb
